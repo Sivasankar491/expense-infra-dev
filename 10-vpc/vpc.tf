22d16530aa996639fa6a30fa1610ef8d61760368
Sankar491 <sivasankarrcks@gmail.com>
2024-10-04 22:25:22 +0530

    Created VPC

---
+++ 10-vpc/vpc.tf
@@ -0,0 +1,11 @@
+module "vpc" {
+  # source      = "../Terraform-vpc-module"
+  source = "git::https://github.com/Sivasankar491/Terraform-vpc-module.git?ref=main"
+  project     = var.project_name
+  environment = var.environment_name
+  common_tags = var.common_tags
+  public_subnet_cidrs = var.public_subnet_cidrs
+  private_subnet_cidrs = var.private_subnet_cidrs
+  database_subnet_cidrs = var.database_subnet_cidrs
+  is_peering_required = true
+}
