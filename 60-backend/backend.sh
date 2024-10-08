5a4fd02dcac269e2170a819e62540d9c1e9df6eb
Sankar491 <sivasankarrcks@gmail.com>
2024-10-08 13:19:36 +0530

    m

--- 60-backend/backend.sh
+++ 60-backend/backend.sh
@@ -6,3 +6,4 @@ echo "component is $component, environment is $environment"
 dnf install ansible -y
 
 ansible-pull -i localhost, -U https://github.com/Sivasankar491/expense-ansible-roles.git main.yaml -e component=$component -e environment=$environment
+
